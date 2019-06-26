1cfabd1f42d67177fc66d9594136cf23a374574e
Kenge-dingxin <pgkusn@gmail.com>
2019-06-26 15:03:45 +0800

    變更食品業第二版路徑 (移至 dev)

---
+++ dsc/dev/solution/WB002553/components/case.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="case">
+        <div class="case-illust" :style="'background-image:url(images/case/case_' + id + '.jpg)'"></div>
+        <div class="case-content">
+            <h2>{{content.company}}</h2>
+            <h3>{{content.category}}</h3>
+            <vue-text-dot :msg="content.desc"></vue-text-dot>
+            <a :href="content.url" target="_blank">繼續閱讀</a>
+        </div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    props: ['id'],
+    computed: {
+        cases: function () {
+            return store.state.result.cases;
+        },
+        content: function () {
+            return this.cases.filter(function (val) {
+                return val.id === this.id;
+            }.bind(this))[0];
+        }
+    },
+}
+</script>
