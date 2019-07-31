9ee5273f1526d9d64950c81c7344f4f4f9e1658a
Kenge-dingxin <pgkusn@gmail.com>
2019-07-31 19:38:56 +0800

    HR 文章改版

--- dsc/js/common.js
+++ dsc/js/common.js
@@ -138,59 +138,79 @@
     }
 })();
 
+// add production/no-production class
 (function () {
-    if (location.hostname === 'www.digiwin.com') {
-        document.querySelector('html').classList.add('production');
-    }
+    let str = location.hostname === 'www.digiwin.com' ? 'production' : 'no-production';
+    document.querySelector('html').classList.add(str);
 })();
 
+// 修改手機版電話號碼(與後台電腦版連動)
 $(function () {
-    // 修改手機版電話號碼(與後台電腦版連動)
-    (function () {
-        let target = document.querySelector('#toolbar .p-2');
-        if (target) {
-            let phoneNum = target.innerText.trim();
-            document.querySelector('.xz-fudong a').setAttribute('href', 'tel:' + phoneNum);
-        }
-    })();
+    let target = document.querySelector('#toolbar .p-2');
+    if (target) {
+        let phoneNum = target.innerText.trim();
+        document.querySelector('.xz-fudong a').setAttribute('href', 'tel:' + phoneNum);
+    }
 
-    // 設置文章閱讀權限及插入訂閱表單
-    // 表單標題清單：/tw/dsc/assets/lineMember/form.json
-    (function () {
-        let currentUrl = location.pathname.replace(/(.html|.htm)$/, '');
+});
 
-        // 加入閱讀權限的文章
-        let loginUrls = [
-            '/tw/dsc/dev/demo/line_member',
-            '/tw/blog/3/index/1897.html',
-            '/tw/blog/3/index/1899.html',
-            '/tw/blog/3/index/1911.htm',
-            '/tw/blog/3/index/1913.htm',
-            '/tw/blog/3/index/1914.htm',
-            '/tw/blog/3/index/1936.html',
-        ];
+// 設置文章閱讀權限及插入訂閱表單
+// 表單標題清單：/tw/dsc/assets/lineMember/form.json
+$(function () {
+    let currentUrl = location.pathname.replace(/(.html|.htm)$/, '');
 
-        // 比對網址成功後載入對應的外部連結
-        let loginResult = loginUrls.find(function (item) {
-            item = item.replace(/(.html|.htm)$/, '');
-            let regex = new RegExp(item + '$');
-            return currentUrl.search(regex) !== -1;
-        });
+    // 加入閱讀權限的文章
+    let loginUrls = [
+        '/tw/dsc/dev/demo/line_member',
+        '/tw/blog/3/index/1897.html',
+        '/tw/blog/3/index/1899.html',
+        '/tw/blog/3/index/1911.htm',
+        '/tw/blog/3/index/1913.htm',
+        '/tw/blog/3/index/1914.htm',
+        '/tw/blog/3/index/1936.html',
+    ];
 
-        if (!loginResult) return;
+    // 比對網址成功後載入對應的外部連結
+    let loginResult = loginUrls.find(function (item) {
+        item = item.replace(/(.html|.htm)$/, '');
+        let regex = new RegExp(item + '$');
+        return currentUrl.search(regex) !== -1;
+    });
 
-        let head = document.querySelector('head');
-        let style = document.createElement('link');
-        style.href = '/tw/dsc/assets/lineMember/css/lineMember.css';
-        style.rel = 'stylesheet';
-        head.appendChild(style);
+    if (!loginResult) return;
 
-        let preload = document.createElement('link');
-        preload.href = '/tw/dsc/assets/lineMember/images/lineMember/btn-h.png';
-        preload.rel = 'preload';
-        preload.as = 'image';
-        head.appendChild(preload);
+    let head = document.querySelector('head');
+    let style = document.createElement('link');
+    style.href = '/tw/dsc/assets/lineMember/css/lineMember.css';
+    style.rel = 'stylesheet';
+    head.appendChild(style);
 
-        $.getScript('/tw/dsc/assets/lineMember/js/lineMember.min.js');
-    })();
+    let preload = document.createElement('link');
+    preload.href = '/tw/dsc/assets/lineMember/images/lineMember/btn-h.png';
+    preload.rel = 'preload';
+    preload.as = 'image';
+    head.appendChild(preload);
+
+    $.getScript('/tw/dsc/assets/lineMember/js/lineMember.min.js');
+});
+
+// 解決主選單擋住文章內容的問題
+$(function () {
+    let mouseenterHandler = function () {
+        document.querySelector('header').style.zIndex = 100;
+    };
+
+    let mouseleaveHandler = function () {
+        setTimeout(() => {
+            if (document.querySelector('#qcTopNavSheetWrapper').style.display === 'none') {
+                document.querySelector('header').removeAttribute('style');
+            }
+        }, 500);
+    };
+
+    if (location.hostname !== 'www.digiwin.com') return;
+    document.querySelector('.nav').addEventListener('mouseenter', mouseenterHandler);
+    document.querySelector('.nav').addEventListener('mouseleave', mouseleaveHandler);
+    document.querySelector('#qcTopNavSheetWrapper').addEventListener('mouseenter', mouseenterHandler);
+    document.querySelector('#qcTopNavSheetWrapper').addEventListener('mouseleave', mouseleaveHandler);
 });
