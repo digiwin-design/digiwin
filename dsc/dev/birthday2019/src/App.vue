1ac4d4f3f37dccca6f969bea87f45fe0576e4bb1
Kenge-dingxin <pgkusn@gmail.com>
2019-08-05 19:10:00 +0800

    新增生日賀卡

---
+++ dsc/dev/birthday2019/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="wrapper">
+        <Header></Header>
+        <MenuMask></MenuMask>
+        <div class="main">
+            <Desk></Desk>
+        </div>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+import './assets/js/modernizr-custom';
+import './assets/js/mobile-detect';
+import './assets/js/mobile-detect-modernizr';
+import 'jquery-popup-overlay';
+
+import Header from './components/Header.vue';
+import MenuMask from './components/MenuMask.vue';
+import Footer from './components/Footer.vue';
+import Desk from './components/Desk.vue';
+
+export default {
+    name: 'app',
+    components: {
+        Header,
+        MenuMask,
+        Footer,
+        Desk,
+    },
+    mounted() {
+        this.$store.commit('setDevice', new MobileDetect(window.navigator.userAgent).mobile());
+    },
+}
+</script>
+
+<style lang="scss" src="@/assets/sass/style.scss"></style>
---
+++ dsc/dev/birthday2019/src/components/Popup.vue
@@ -0,0 +1,66 @@
+<template>
+    <div id="popup" class="popup">
+        <slot></slot>
+        <button class="popup_close"></button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        popupInit() {
+            $('#popup').popup({
+                opacity: 0.3,
+                transition: 'all 0.3s',
+                scrolllock: true,
+                onclose: () => {
+                    this.$store.commit('togglePopup', false);
+                }
+            });
+        },
+    },
+    mounted() {
+        this.popupInit();
+    },
+}
+</script>
+
+
+<style lang="scss" scoped>
+.popup {
+    position: absolute !important;
+    top: 50%;
+    left: 50%;
+    margin: auto;
+    max-width: 1067px;
+    max-height: 621px;
+    width: 90%;
+    height: 90%;
+    border: 1px solid #7bc9ff;
+    border-radius: 5px;
+    background-color: #232121;
+    opacity: 0;
+    transform: translate(-50%, -50%);
+    @media (max-width: 1200px) {
+        max-height: none;
+    }
+    &_close {
+        position: absolute;
+        top: -16px;
+        right: -16px;
+        z-index: 1;
+        width: 42px;
+        height: 42px;
+        border: none;
+        background-color: transparent;
+        background-image: url("~@/assets/images/popup-close.png");
+        background-position: 50%;
+        background-size: contain;
+        background-repeat: no-repeat;
+        cursor: pointer;
+        &:focus {
+            outline: none;
+        }
+    }
+}
+</style>
