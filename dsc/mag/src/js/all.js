3c9532e19af2291631c984dcfc9802b18b069cd8
kenge <pgkusn@gmail.com>
2019-10-16 07:51:28 +0800

    update mag

--- dsc/mag/src/js/all.js
+++ dsc/mag/src/js/all.js
@@ -118,13 +118,10 @@ document.addEventListener('click', function () {
     const el = event.target;
     if (el.tagName === 'A' && el.classList.contains('js-nav')) {
         event.preventDefault();
-        let delay = el.dataset.delay ? el.dataset.delay : 0;
         let target = el.getAttribute('href') || el.getAttribute('xlink:href') || el.dataset.target;
-        setTimeout(() => {
-            let offset = document.querySelector('.page-submenu') && document.querySelector('.page-submenu').offsetHeight;
-            let targetPos = document.querySelector(target).offsetTop;
-            let finalPos = offset ? targetPos - offset : targetPos;
-            window.scroll({ top: finalPos, left: 0, behavior: 'smooth' });
-        }, delay);
+        let offset = document.querySelector('.page-submenu') && document.querySelector('.page-submenu').offsetHeight;
+        let targetPos = document.querySelector(target).getBoundingClientRect().top + window.pageYOffset;
+        let finalPos = offset ? targetPos - offset : targetPos;
+        window.scroll({ top: finalPos, left: 0, behavior: 'smooth' });
     }
 });
