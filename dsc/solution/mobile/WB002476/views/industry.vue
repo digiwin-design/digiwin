99ea4850ed54dad4918c0220151a89e2c686088d
Kenge-dingxin <pgkusn@gmail.com>
2019-10-21 18:13:02 +0800

    新增機械業手機版

---
+++ dsc/solution/mobile/WB002476/views/industry.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="industry">
+        <main-header>
+            <template slot="content"></template>
+        </main-header>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    name: 'industry',
+    computed: {
+        result() {
+            return store.state.result;
+        }
+    },
+}
+</script>
