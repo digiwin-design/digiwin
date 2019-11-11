3e8455e72030c84d71a982183eded26af7891e5f
Kenge-dingxin <pgkusn@gmail.com>
2019-11-11 18:10:27 +0800

    update mes cpl v3 & mag

--- dsc/mag/components/mag201909/robot.vue
+++ dsc/mag/components/mag201909/robot.vue
@@ -4,7 +4,7 @@
         target="_blank"
         class="robot"
         :class="{ active: active }"
-        @mouseenter="isHover=true"
+        @mouseenter="onMouseEnter"
         @mouseleave="onMouseleave"
     ></a>
 </template>
@@ -15,10 +15,18 @@ module.exports = {
         return {
             debug: false,
             active: false,
-            sec: 0,
+            currentTime: 0,
             isHover: false,
             timer: null,
         }
+    },
+    computed: {
+        isMobile() {
+            return store.state.isMobile;
+        },
+        stayTime: function () {
+            return this.isMobile ? 5 : 7;
+        },
     },
     watch: {
         isHover: function (val) {
@@ -39,23 +47,28 @@ module.exports = {
             if (this.debug) return;
             clearTimeout(this.timer);
             this.timer = setTimeout(function () {
-                if (this.sec < 7) {
+                if (this.currentTime < this.stayTime) {
                     this.active = true;
                     if (!this.isHover) {
-                        this.sec++;
+                        this.currentTime++;
                         this.setTimer();
                     }
                 }
                 else {
                     this.active = false;
-                    this.sec = 0;
+                    this.currentTime = 0;
                     setTimeout(this.setTimer, 3000);
                 }
             }.bind(this), 1000);
         },
+        onMouseEnter: function () {
+            if (!this.isMobile) {
+                this.isHover = true;
+            }
+        },
         onMouseleave: function () {
             this.isHover = false;
-            this.sec = 0;
+            this.currentTime = 0;
             this.setTimer();
         }
     },
@@ -65,7 +78,7 @@ module.exports = {
 }
 </script>
 
-<style scoped>
+<style>
 .robot {
     position: fixed;
     bottom: 0;
@@ -74,10 +87,21 @@ module.exports = {
     width: 370px;
     height: 370px;
     transition: transform 1s;
-    transform: translateX(-100%);
+    transform: translateX(-100%) rotate(30deg);
+}
+@media (max-width: 768px) {
+    .robot {
+        transform-origin: 30% 50%;
+        transform: translateX(-100%) scale(0.5) rotate(30deg);
+    }
 }
 .robot.active {
-    transform: translateX(-110px);
+    transform: translateX(-110px) rotate(30deg);
+}
+@media (max-width: 768px) {
+    .robot.active {
+        transform: translateX(-110px) scale(0.5) rotate(30deg);
+    }
 }
 .robot [data-name="arm"] {
     transform-origin: 202px 160px;
@@ -87,9 +111,13 @@ module.exports = {
 }
 .robot [data-name="lower-arm"] {
     transform-origin: 252px 212px;
+    transform: rotate(30deg);
 }
 .robot.active [data-name="lower-arm"] {
     animation: lowerArmAn 0.5s 1s infinite alternate;
+}
+.robot [data-name="ball"] {
+    transform: translate(53px, 20px);
 }
 .robot.active [data-name="ball"] {
     animation: ballAn 0.5s 1s infinite alternate ease-out;
