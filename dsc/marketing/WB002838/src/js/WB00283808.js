66f4403667eeeae382da0614cf3253fb8cc3f65f
kenge <pgkusn@gmail.com>
2019-12-14 00:52:19 +0800

    fix ad bug

--- dsc/marketing/WB002838/src/js/WB00283808.js
+++ dsc/marketing/WB002838/src/js/WB00283808.js
@@ -6,6 +6,6 @@ if (md.mobile()) {
 // banner link
 dayjs.extend(dayjs_plugin_isBetween);
 let showAd = dayjs().isBetween('2019-12-09', '2019-12-13');
-if (!showAd) {
-    document.querySelector('#banner-link').style.display = 'none';
+if (showAd) {
+    document.querySelector('#banner-link').style.display = 'block';
 }
