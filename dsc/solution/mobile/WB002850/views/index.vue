63daa5dc628b5fc2b6ff341fe9f0a9f8e9a5b03d
Kenge-dingxin <pgkusn@gmail.com>
2019-12-18 18:47:37 +0800

    新增醫美業手機版

---
+++ dsc/solution/mobile/WB002850/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="index">
+        <slider-banner-s :slides="result.banner"></slider-banner-s>
+
+        <template v-if="isReady">
+
+        </template>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    name: 'WB002850',
+    computed: {
+        result() {
+            return store.state.result.index;
+        },
+        isReady() {
+            return store.state.isReady;
+        },
+    },
+}
+</script>
